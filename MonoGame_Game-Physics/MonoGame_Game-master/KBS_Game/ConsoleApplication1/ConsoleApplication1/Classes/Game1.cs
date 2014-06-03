53c697efd43d502a64a07147eaac5e67c7c8f715
BrendanHurt <brendan.Frodado@gmail.com>
2014-06-03 16:20:37 -0700

    Game Build
    Now with 100% more player death

---
+++ MonoGame_Game-Physics/MonoGame_Game-master/KBS_Game/ConsoleApplication1/ConsoleApplication1/Classes/Game1.cs
@@ -0,0 +1,77 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ConsoleApplication1.Classes;
+using Microsoft.Xna.Framework.Input;
+
+namespace ConsoleApplication1
+{
+    // Shell for a basic MonoGame game.
+
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics; // Came with the code, don't mess with it.
+        SpriteBatch spriteBatch; // We use these to draw textures, every texture will "belong" to one of these.
+
+        Texture2D PlayerTexture; // texture for testing
+        Player PlayerOne; //sprite for testing, see Sprite.cs
+
+        //Level LevelOne;
+
+        //Constructor for the game
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content"; // This is a folder inside our project directory where all the images and shit go.
+        }
+
+        //Don't mess with this yet.
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        //This is where we load in all our textures and stuff.
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            PlayerTexture = Content.Load<Texture2D>("CRATE_5");
+            PlayerOne = new Player(PlayerTexture, new Vector2(50, 50), spriteBatch); //Again, see Sprite.cs
+            //Destroyer = new TerrainDestroyer(Content.Load<Texture2D>("deform"), new Vector2(50, 50), spriteBatch);
+            //LevelOne = new Level(Content.Load<Texture2D>("bg"), spriteBatch);
+
+            this.IsMouseVisible = true;
+        }
+
+        protected override void UnloadContent()
+        {
+          
+        }
+
+        //This is where game logic will  be updated.
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            if (PlayerOne.IsAlive)
+            {
+                PlayerOne.Update(gameTime);
+            }
+        }
+        
+        //This is where drawing shit happens. 
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue); //Blue background
+            spriteBatch.Begin(); //Everything that belongs to "spriteBatch" will be drawn between the .begin() & .end()
+            base.Draw(gameTime);
+            if (PlayerOne.IsAlive)
+            {
+                PlayerOne.Draw(); //This actually draws the test image.
+            }
+            spriteBatch.End();
+        }
+    }
+}
---
+++ MonoGame_Game-Physics/MonoGame_Game-master/KBS_Game/ConsoleApplication1/ConsoleApplication1/Classes/Player.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ConsoleApplication1
+{
+    public class Player : Sprite
+    {
+        private bool OnGround = false;
+        public bool IsAlive = true;
+        private int JetpackFuel = 100;
+        private int Health = 100;
+        private float Velocity = 0;
+
+        public Vector2 Movement { get; set; }
+
+        public Player (Texture2D texture, Vector2 position, SpriteBatch spritebatch)
+            : base(texture, position, spritebatch)
+        {
+
+        }
+
+        public void ApplyGravity(GameTime gameTime)
+        {
+            if (!OnGround)
+            {
+                float i = 1;
+                Velocity += 0.1f * i;
+                Position += new Vector2(0, Velocity);
+            }
+            else
+            {
+                Movement = new Vector2(0, 0);
+                OnGround = true;
+            }
+        }
+
+        public void Jump()
+        {
+            if (Position.Y >= 1 && OnGround)
+            {
+                OnGround = false;
+                Movement += new Vector2(0, -10);
+
+                Velocity = -5;
+            }
+        }
+
+        public void JetPack()
+        {
+            if (JetpackFuel >= 0)
+            {
+                Velocity = 0;
+                OnGround = false;
+                Movement += new Vector2(0, -1);
+                JetpackFuel -= 1;
+            }
+        }
+
+        public void TakeDamage()
+        {
+            KeyboardState keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Keys.D)) { Health -= 10; }
+            if (Health <= 0)
+            {
+                IsAlive = false;
+            }
+        }
+
+        public void FireWeapon()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            TakeDamage();
+
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Left)) { Movement += new Vector2(-1, 0); }
+            if (keyboardState.IsKeyDown(Keys.Right)) { Movement += new Vector2(1, 0); }
+            if (keyboardState.IsKeyDown(Keys.Up)) { Jump(); }
+            if (keyboardState.IsKeyDown(Keys.Space)) { JetPack(); }
+
+            Movement -= Movement * new Vector2(.1f, .1f);
+            Position += Movement * (float)gameTime.ElapsedGameTime.TotalMilliseconds / 15;
+            
+            //ApplyGravity(gameTime);
+
+           
+        }
+    }
+}
